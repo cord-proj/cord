373ca4046ee11020b7bd02dbfa7070eccd1ea52c
Pete Hayes <pete@hayes.id.au>
2019-10-18 18:22:36 +0100

    WIP - Finished Simple producer

--- generators/src/simple.rs
+++ generators/src/simple.rs
@@ -1,10 +1,17 @@
 use super::{Client, Factory, MessageStream, CONSUMERS_PER_CLIENT};
-use client::errors::{Error, ErrorKind};
+use client::errors::Error;
 use futures::{Future, Stream};
 use message::Message;
 use pattern_matcher::Pattern;
 use rand::{self, rngs::ThreadRng, Rng};
-use std::{net::SocketAddr, result};
+use std::{
+    net::SocketAddr,
+    result,
+    sync::{
+        atomic::{AtomicU64, Ordering},
+        Arc,
+    },
+};
 use tokio::prelude::Async;
 
 const MESSAGE: &str = "message";
@@ -21,6 +28,7 @@ pub struct SimpleFactory {
 struct SimpleProducer {
     namespace: Pattern,
     message: String,
+    msg_count: Arc<AtomicU64>,
 }
 
 impl SimpleFactory {
@@ -59,11 +67,23 @@ impl Factory for SimpleFactory {
     where
         F: Fn(MessageStream) -> MessageStream + 'static,
     {
+        // Get or generate the provider namespace for this client
         let provider_ns = self
             .provider_ns
             .pop()
             .unwrap_or_else(|| self.gen_namespace());
-        let subscriber_ns = Vec::new();
+
+        // Get or generate the subscriber namespaces for this client
+        let mut subscriber_ns = self.subscriber_ns.clone();
+        // We need a namespace per consumer. If `subscriber_ns` is not full,
+        // top it up with new namespaces.
+        for _ in subscriber_ns.len()..self.consumers_per_client as usize {
+            let ns = self.gen_namespace();
+            // For any new namespace we subscribe to, we also need a provider.
+            // Thus we add every new namespace to the "needs provider" list.
+            self.provider_ns.push(ns.clone());
+            subscriber_ns.push(ns);
+        }
 
         // The subscriber buffer should only ever contain as many namespaces as
         // there are consumers.
@@ -74,23 +94,31 @@ impl Factory for SimpleFactory {
         // Now that we are providing provider_ns, the next client can subscribe
         // to it.
         self.subscriber_ns.push(provider_ns.clone());
+
+        // Setup our atomic message counter to compare what we sent and what we
+        // received.
+        let counter = Arc::new(AtomicU64::new(0));
+
         Client::new(
             addr,
             provider_ns.clone(),
-            decorator(Box::new(SimpleProducer::new(provider_ns, MESSAGE))),
+            decorator(Box::new(SimpleProducer::new(
+                provider_ns,
+                MESSAGE,
+                counter.clone(),
+            ))),
             subscriber_ns,
-            |subscriber| {
+            move |subscriber| {
+                let c = counter.clone();
                 Box::new(
                     subscriber
                         .fold(0, |acc, _| Ok::<_, Error>(acc + 1))
-                        .and_then(|acc| {
-                            if acc == 1 {
+                        .and_then(move |acc| {
+                            if acc == c.load(Ordering::Relaxed) {
                                 Ok(())
                             } else {
-                                Err(Error::from_kind(ErrorKind::Msg(format!(
-                                    "Expected to receive {} messages, got {}",
-                                    1, acc
-                                ))))
+                                Err(format!("Expected to receive {} messages, got {}", 1, acc)
+                                    .into())
                             }
                         }),
                 )
@@ -104,10 +132,15 @@ impl Factory for SimpleFactory {
 }
 
 impl SimpleProducer {
-    fn new<S: Into<String>>(namespace: Pattern, message: S) -> SimpleProducer {
+    fn new<S: Into<String>>(
+        namespace: Pattern,
+        message: S,
+        counter: Arc<AtomicU64>,
+    ) -> SimpleProducer {
         SimpleProducer {
             namespace,
             message: message.into(),
+            msg_count: counter,
         }
     }
 }
@@ -117,6 +150,8 @@ impl Stream for SimpleProducer {
     type Error = Error;
 
     fn poll(&mut self) -> result::Result<Async<Option<Self::Item>>, Self::Error> {
+        // Increment the message count for later comparison
+        self.msg_count.fetch_add(1, Ordering::Relaxed);
         Ok(Async::Ready(Some(Message::Event(
             self.namespace.clone(),
             self.message.clone(),
